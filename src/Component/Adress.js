4ddbc2ffb8a8a341622f7365b03fc9439d25065c
khayreddinekhlifi <khayrikhlifi21@gmail.com>
2022-11-26 11:06:37 +0100

    first commit

---
+++ src/Component/Adress.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Adress() {
+  return (
+    <div>
+      <h1>khayrikhlifi@gmail.com</h1>
+    </div>
+  );
+}
---
+++ src/Component/ProfilePhoto.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ProfilePhoto() {
+  return (
+    <div>
+      <img
+        className="fit-picture"
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+        alt="Grapefruit slice atop a pile of other slices"
+      />
+    </div>
+  );
+}
